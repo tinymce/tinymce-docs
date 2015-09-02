dfbf8e826fbfa42f75a16812afbd647e30bfa288
Joshua Haines <josh.j.haines@gmail.com>
2015-09-02 12:28:30 +1000

    working scrolling sidebar

---
+++ js/navigation.js
@@ -0,0 +1,14 @@
+(function(){
+	var items = $('.navigationlink');
+
+	function clickEvent(event) {
+		$('html, body').animate({
+			scrollTop: $('#' + event.data).offset().top
+		}, 2000);
+	}
+
+	for (var i = 0; i < items.length; i++) {
+		var selector = $(items[i]).attr('data-id');
+		$(items[i]).click(selector, clickEvent);
+	}
+})();

beb3e6e6287d473c3d84e54e330ed40ecd30dbbd
Kilian Ciuffolo <kilian.ciuffolo@gmail.com>
2015-11-27 19:16:25 -0800

    added include_remote tag :beer:

--- init.sh
+++ init.sh
@@ -14,18 +14,6 @@ else
   cp _config-local.sample.yml _config-local.yml
 fi
 
-echo " > downloading html includes 1/4"
-curl -s https://www.tinymce.com/_docs/menu.html > _includes/menu.html
-
-echo " > downloading html includes 2/4"
-curl -s https://www.tinymce.com/_docs/menu_demo.html > _includes/menu_demo.html
-
-echo " > downloading html includes 3/4"
-curl -s https://www.tinymce.com/_docs/footer.html > _includes/footer.html
-
-echo " > downloading html includes 4/4"
-curl -s https://www.tinymce.com/_docs/favicons.html > _includes/favicons.html
-
 echo " > creating dummy API ref data file"
 echo [] > _data/nav_api.json
 

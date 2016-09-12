68085668f4dfa1400624b759304f3ecaeb290411
Spocke <spocke@moxiecode.com>
2016-09-12 15:20:10 +0200

    added local api ref import

---
+++ _scripts/api-reference-local.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+API_TMPDIR="/tmp/tinymce-$API_VERSION"
+
+echo -e "\n > importing data files for tinymce api reference: local\n"
+
+rm -rf "$API_TMPDIR"
+mkdir "$API_TMPDIR"
+moxiedoc "../tinymce-stash/js/tinymce/classes" -t tinymcenext -o "$API_TMPDIR/tinymce-api-reference.zip"
+unzip -o "$API_TMPDIR/tinymce-api-reference.zip"
+
+echo ""

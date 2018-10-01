0fae8de985eb28e035c1d1520c2bbb799c895557
Mike Chau <mike.chau@ephox.com>
2018-10-01 10:34:17 +1000

    update screen shot

--- _includes/codepens/full-featured/index.js
+++ _includes/codepens/full-featured/index.js
@@ -1,7 +1,7 @@
 tinymce.init({
   selector: 'textarea',
   height: 500,
-  plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker contextmenu colorpicker textpattern help',
+  plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists textcolor wordcount tinymcespellchecker a11ychecker imagetools mediaembed  linkchecker colorpicker textpattern help',
   toolbar1: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
   image_advtab: true,
   templates: [

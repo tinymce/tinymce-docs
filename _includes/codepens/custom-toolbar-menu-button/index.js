d4b01566c3f8d59e0f37bd0f9061288972e216f2
Mike Chau <mike.chau@ephox.com>
2018-10-01 15:26:37 +1000

    add new toolbar menu button demo

--- _includes/codepens/custom-toolbar-menu-button/index.js
+++ _includes/codepens/custom-toolbar-menu-button/index.js
@@ -2,30 +2,48 @@ tinymce.init({
   selector: 'textarea',
   height: 500,
   toolbar: 'mybutton',
-  menubar: false,
-    content_css: [
-    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-    '//www.tiny.cloud/css/codepen.min.css'],
+
+  content_css: [
+  '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+  '//www.tiny.cloud/css/codepen.min.css'],
   
   setup: function(editor) {
-    editor.ui.registry.addButton('mybutton', {
-      type: 'menubutton',
+
+    /* example, adding a toolbar menu button */
+    editor.ui.registry.addMenuButton('mybutton', {
       text: 'My button',
-      icon: false,
-      menu: [{
-        text: 'Menu item 1',
-        onclick: function() {
-          editor.insertContent('&nbsp;<strong>Menu item 1 text inserted here!</strong>&nbsp;');
-        }
-      }, {
-        text: 'Menu item 2',
-        menu: [{
-          text: 'Submenu item 1',
-          onclick: function() {
-            editor.insertContent('&nbsp;<em>Submenu item 1 text inserted here!</em>&nbsp;');
-          }
-        }]
-      }]
+      fetch: (callback) => {
+        const items = [
+          {
+            type: 'menuitem',
+            text: 'Menu item 1',
+            onAction: () => editor.insertContent('&nbsp;<em>You clicked menu item 1!</em>')
+          },
+          {
+            type: 'menuitem',
+            text: 'Menu item 2',
+            icon: 'user',
+            getSubmenuItems: () => {
+              return [
+                {
+                  type: 'menuitem',
+                  text: 'Sub menu item 1',
+                  icon: 'unlock',
+                  onAction: () => editor.insertContent('&nbsp;<em>You clicked Sub menu item 1!</em>')
+                },
+                {
+                  type: 'menuitem',
+                  text: 'Sub menu item 2',
+                  icon: 'lock',
+                  onAction: () => editor.insertContent('&nbsp;<em>You clicked Sub menu item 2!</em>')
+                }
+              ]
+            }
+          },
+        ];
+        callback(items);
+      }
     });
+
   }
 });

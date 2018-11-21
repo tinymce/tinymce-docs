1a58620a55721f0dc53119b2649c44c972efd681
Lee Newson <lee.newson@tiny.cloud>
2018-11-21 11:52:05 +1000

    TINY-2253: Another context menu codepen fix, to make the link context menu only show when on a link

--- _includes/codepens/contextmenu-section/index.js
+++ _includes/codepens/contextmenu-section/index.js
@@ -17,6 +17,15 @@ tinymce.PluginManager.add('my-example-plugin', function (editor) {
     }
   });
 
+  editor.ui.registry.addMenuItem('unlink', {
+    icon: 'unlink',
+    text: 'Remove link',
+    onAction: () => {
+      console.log('remmove link context menu clicked');
+      alert('remmove link context menu clicked');
+    }
+  });
+
   editor.ui.registry.addContextMenu('image', {
     update: (element) => {
       return !element.src ? [] : ['image'];
@@ -25,7 +34,7 @@ tinymce.PluginManager.add('my-example-plugin', function (editor) {
 
   editor.ui.registry.addContextMenu('link', {
     update: (element) => {
-      return !element.href ? 'link' : 'link unlink openlink';
+      return !element.href ? '' : 'link unlink';
     }
   });
 });

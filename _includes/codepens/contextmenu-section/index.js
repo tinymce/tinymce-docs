cc04b327f0262233d5d6b43bef12583f5bbe4d8c
Lee Newson <lee.newson@tiny.cloud>
2018-11-22 11:11:15 +1000

    TINY-2253: Simplified the context menu codepen to only use string based menu references

--- _includes/codepens/contextmenu-section/index.js
+++ _includes/codepens/contextmenu-section/index.js
@@ -3,44 +3,20 @@ tinymce.PluginManager.add('my-example-plugin', function (editor) {
     icon: 'image',
     text: 'Image',
     onAction: () => {
-      console.log('image context menu clicked');
-      alert('image context menu clicked');
-    }
-  });
-
-  editor.ui.registry.addMenuItem('link', {
-    icon: 'link',
-    text: 'Link',
-    onAction: () => {
-      console.log('link context menu clicked');
-      alert('link context menu clicked');
-    }
-  });
-
-  editor.ui.registry.addMenuItem('unlink', {
-    icon: 'unlink',
-    text: 'Remove link',
-    onAction: () => {
-      console.log('remmove link context menu clicked');
-      alert('remmove link context menu clicked');
+      console.log('context menu clicked');
+      alert('context menu clicked');
     }
   });
 
   editor.ui.registry.addContextMenu('image', {
     update: (element) => {
-      return !element.src ? [] : ['image'];
-    }
-  });
-
-  editor.ui.registry.addContextMenu('link', {
-    update: (element) => {
-      return !element.href ? '' : 'link unlink';
+      return !element.src ? '' : 'image';
     }
   });
 });
 
 tinymce.init({
   selector: "textarea",
-  contextmenu: "link image",
+  contextmenu: "image",
   plugins: 'my-example-plugin'
 });

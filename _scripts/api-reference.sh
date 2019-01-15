6ad4df84169b7fa21dc73562e8ad58748c4fb790
Mike Chau <mike.chau@ephox.com>
2019-01-15 15:39:03 +1000

    TINY-3017: re-implement api-reference to load from a temporary s3 bucket

--- _scripts/api-reference.sh
+++ _scripts/api-reference.sh
@@ -3,16 +3,19 @@
 set -e
 
 API_VERSION="$(cat .api-version)"
-TARBALL_URL="https://codeload.github.com/tinymce/tinymce/tar.gz/$API_VERSION"
+#TARBALL_URL="https://codeload.github.com/tinymce/tinymce/tar.gz/$API_VERSION"
+#Release candidate temporary s3 tarball, once we go stable the above link resumes
+TARBALL_URL="https://devpreview-tiny-cloud.s3.amazonaws.com/download/$API_VERSION.tar.gz"
 API_TMPDIR="/tmp/tinymce-$API_VERSION"
 
 echo -e "\n > importing data files for tinymce api reference: $API_VERSION\n"
 
-#rm -f _data/nav_api.json
-#rm -rf "$API_TMPDIR"
+rm -f _data/nav_api.json
+rm -rf "$API_TMPDIR"
+
 mkdir "$API_TMPDIR"
 curl -s "$TARBALL_URL" | tar xzf - -C "$API_TMPDIR" --strip-components 1
-moxiedoc "$API_TMPDIR/src/core/main/ts" "$API_TMPDIR/src/ui/main/ts" -t tinymcenext -o "$API_TMPDIR/tinymce-api-reference.zip"
+moxiedoc "$API_TMPDIR/src/core/main/ts" -t tinymcenext -o "$API_TMPDIR/tinymce-api-reference.zip"
 unzip -o "$API_TMPDIR/tinymce-api-reference.zip"
 
 echo ""

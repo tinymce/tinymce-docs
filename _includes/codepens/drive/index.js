fefbb2508c497f8329fc0ce0d6dcbefb689f83b0
Lee Newson <lee.newson@tiny.cloud>
2019-01-22 11:45:26 +1000

    TINY-3174: Fixed the plugin drive demo using ES6 syntax

--- _includes/codepens/drive/index.js
+++ _includes/codepens/drive/index.js
@@ -8,13 +8,9 @@ tinymce.init({
   ],
   height: 600,
   imagetools_cors_hosts: ['picsum.photos'],
-  tinydrive_token_provider: (success, failure) => {
+  tinydrive_token_provider: function (success, failure) {
     success({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.Ks_BdfH4CWilyzLNk8S2gDARFhuxIauLa8PwhdEQhEo' });
   },
   tinydrive_demo_files_url: '{{ site.baseurl }}/demo/tiny-drive-demo/demo_files.json',
-  toolbar: 'insertfile image link | code',
-  content_css: [
-    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-    '//www.tiny.cloud/css/codepen.min.css'
-  ]
+  toolbar: 'insertfile image link | code'
 });

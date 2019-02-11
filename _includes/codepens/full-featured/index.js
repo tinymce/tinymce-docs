a5919f781c4b5f2ea4a0ca19db4797e130dbcc28
Lee Newson <lee.newson@tiny.cloud>
2019-02-11 12:12:03 +1000

    TINY-2985: Added configuration for page embed, permanent pen and comments 2.0 to the full featured demo

--- _includes/codepens/full-featured/index.js
+++ _includes/codepens/full-featured/index.js
@@ -1,7 +1,7 @@
 tinymce.init({
   selector: 'textarea#full-featured',
-  plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount tinymcespellchecker a11ychecker imagetools textpattern help',
-  toolbar: 'formatselect | bold italic strikethrough forecolor backcolor | link | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent  | removeformat',
+  plugins: 'print preview fullpage powerpaste searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount tinymcespellchecker a11ychecker imagetools textpattern help formatpainter permanentpen pageembed tinycomments',
+  toolbar: 'formatselect | bold italic strikethrough forecolor backcolor permanentpen formatpainter | link image media pageembed | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | removeformat',
   image_advtab: true,
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
@@ -44,7 +44,8 @@ tinymce.init({
   template_cdate_format: '[CDATE: %m/%d/%Y : %H:%M:%S]',
   template_mdate_format: '[MDATE: %m/%d/%Y : %H:%M:%S]',
   image_caption: true,
-  
   spellchecker_dialog: true,
-  spellchecker_whitelist: ['Ephox', 'Moxiecode']
+  spellchecker_whitelist: ['Ephox', 'Moxiecode'],
+  tinycomments_mode: 'embedded',
+  content_style: '.mce-annotation { background: #fff0b7; } .tc-active-annotation {background: #ffe168; color: black; }'
  });

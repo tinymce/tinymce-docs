715eb2822b6fa629d6ceaef51c1d21f2b507c19a
idooo <alex.shteinikov@gmail.com>
2019-02-20 15:14:05 +1000

    DOC-237 linkchecker

--- _scripts/check-links.sh
+++ _scripts/check-links.sh
@@ -4,4 +4,4 @@
 # https://www.notion.so/tinycloud/Check-for-broken-links-4876019acee44cc88c5082e447c85519
 
 mkdir -p ./_report/report
-linkchecker -r 2 -t 20 http://stagings.tiny.cloud/docs/ --user-agent=linkchecker --output=html > _report/report/index.html || true
+linkchecker -r 0 -t 20 http://staging.tiny.cloud/docs/ --user-agent=linkchecker --output=html > _report/report/index.html || true

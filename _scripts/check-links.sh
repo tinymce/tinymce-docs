d0019fa193796b866b83698f4b01190864c22024
idooo <alex.shteinikov@gmail.com>
2019-02-21 14:23:51 +1000

    fixes for the build

--- _scripts/check-links.sh
+++ _scripts/check-links.sh
@@ -4,4 +4,4 @@
 # https://www.notion.so/tinycloud/Check-for-broken-links-4876019acee44cc88c5082e447c85519
 
 mkdir -p ./_report/report
-linkchecker -r 2 -t 10 $S3_BUCKET/docs/ --check-extern --output=html > _report/report/index.html
+linkchecker -r 2 -t 10 https://staging.tiny.cloud/docs/ --check-extern --output=html > _report/report/index.html || true

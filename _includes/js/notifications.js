2edfd1289f8d23de7c7e58feda54086422f64a89
grade-zero <b.cream87@gmail.com>
2019-06-17 10:15:04 +1000

    WEBSITE-1582 - News from contentful

--- _includes/js/notifications.js
+++ _includes/js/notifications.js
@@ -7,37 +7,30 @@
         return;
     }
 
-    function setNews(latest) {
+    function setResultToNewsBar(latest) {
         $(".tiny-news-text").text(latest.text);
         var newsLink = $(".tiny-news-link");
         newsLink.attr("href", latest.url);
         newsLink.text(latest.urlText);
     }
 
-    function go() {
-        var storedNews = localStorage.getItem("latestTinyNews");
-        console.log("storedNews", storedNews);
-        if (storedNews === null) {
-            console.log("no news in storage, fetch some");
-            var url = "https://cdn.jsdelivr.net/npm/contentful@latest/dist/contentful.browser.min.js";
-            $.getScript( url, function() {
-                var client = contentful.createClient({
-                    space: "sn6g75ou164i",
-                    accessToken: "BptrxEYsjeOYo1RSx_-qF_UVkn5HDs_MeHpG1g0wJx4"
-                });
-                
-                client.getEntries({ order: "-sys.createdAt", limit: 10, content_type: "news" }).then(function(entries) {
-                    console.log("entries", entries);
-                    setNews(entries.items[0].fields);
-                }).catch(function(err) {
-                    console.log(err)
-                });
-            });  
-        } else {
-            console.log("news was stored");
-            var latest = JSON.parse(storedNews).items[0].fields;
-            setNews(latest);
-        }
+    function fetchLatestNews() {
+        var url = "https://cdn.jsdelivr.net/npm/contentful@latest/dist/contentful.browser.min.js";
+        $.getScript( url, function() {
+            var client = contentful.createClient({
+                space: "sn6g75ou164i",
+                accessToken: "BptrxEYsjeOYo1RSx_-qF_UVkn5HDs_MeHpG1g0wJx4"
+            });
+            client.getEntries({
+                order: "-sys.createdAt",
+                limit: 1,
+                content_type: "news"
+            }).then(function(entries) {
+                setResultToNewsBar(entries.items[0].fields);
+            }).catch(function(err) {
+                console.log(err);
+            });
+        });
     }
-    $(document).ready(go);
+    $(document).ready(fetchLatestNews);
 })();

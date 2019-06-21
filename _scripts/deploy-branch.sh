cea4580f5070da259912634d435a7527d6f04fc2
Glenn Penridge <glenn.penridge@tiny.cloud>
2019-06-21 13:03:24 +1000

    Adjusted sync build script path.

--- _scripts/deploy-branch.sh
+++ _scripts/deploy-branch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p ./css
-mkdir -p ./scripts
-mkdir -p ./fonts
+mkdir -p ./_site/css
+mkdir -p ./_site/scripts
+mkdir -p ./_site/fonts
 
 printf "User-agent: * \nDisallow: /\n" > ./robots.txt
 
-curl -o ./css/common.min.css https://staging.tiny.cloud/css/common.min.css -L
-curl -o ./css/docs.min.css https://staging.tiny.cloud/css/docs.min.css -L
-curl -o ./scripts/docs.min.js https://staging.tiny.cloud/scripts/docs.min.js -L
-curl -o ./scripts/common.min.js https://staging.tiny.cloud/scripts/common.min.js -L
-curl -o ./fonts/aileron-ultra-light.woff https://staging.tiny.cloud/fonts/aileron-ultra-light.woff -L
-curl -o ./fonts/aileron-ultra-light.ttf https://staging.tiny.cloud/fonts/aileron-ultra-light.ttf -L
+curl -o ./_site/css/common.min.css https://staging.tiny.cloud/css/common.min.css -L
+curl -o ./_site/css/docs.min.css https://staging.tiny.cloud/css/docs.min.css -L
+curl -o ./_site/scripts/docs.min.js https://staging.tiny.cloud/scripts/docs.min.js -L
+curl -o ./_site/scripts/common.min.js https://staging.tiny.cloud/scripts/common.min.js -L
+curl -o ./_site/fonts/aileron-ultra-light.woff https://staging.tiny.cloud/fonts/aileron-ultra-light.woff -L
+curl -o ./_site/fonts/aileron-ultra-light.ttf https://staging.tiny.cloud/fonts/aileron-ultra-light.ttf -L

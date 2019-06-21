f30198d8f2e61b79dcbb7b108a5a76ac608feb8b
Glenn Penridge <glenn.penridge@tiny.cloud>
2019-06-21 11:55:46 +1000

    Modified branch release script.

--- _scripts/deploy-branch.sh
+++ _scripts/deploy-branch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-mkdir -p ./_essentials/css
-mkdir -p ./_essentials/scripts
-mkdir -p ./_essentials/fonts
+mkdir -p ./css
+mkdir -p ./scripts
+mkdir -p ./fonts
 
-printf "User-agent: * \nDisallow: /\n" > ./_essentials/robots.txt
+printf "User-agent: * \nDisallow: /\n" > ./robots.txt
 
-curl -o ./_essentials/css/common.min.css https://staging.tiny.cloud/css/common.min.css -L
-curl -o ./_essentials/css/docs.min.css https://staging.tiny.cloud/css/docs.min.css -L
-curl -o ./_essentials/scripts/docs.min.js https://staging.tiny.cloud/scripts/docs.min.js -L
-curl -o ./_essentials/scripts/common.min.js https://staging.tiny.cloud/scripts/common.min.js -L
-curl -o ./_essentials/fonts/aileron-ultra-light.woff https://staging.tiny.cloud/fonts/aileron-ultra-light.woff -L
-curl -o ./_essentials/fonts/aileron-ultra-light.ttf https://staging.tiny.cloud/fonts/aileron-ultra-light.ttf -L
+curl -o ./css/common.min.css https://staging.tiny.cloud/css/common.min.css -L
+curl -o ./css/docs.min.css https://staging.tiny.cloud/css/docs.min.css -L
+curl -o ./scripts/docs.min.js https://staging.tiny.cloud/scripts/docs.min.js -L
+curl -o ./scripts/common.min.js https://staging.tiny.cloud/scripts/common.min.js -L
+curl -o ./fonts/aileron-ultra-light.woff https://staging.tiny.cloud/fonts/aileron-ultra-light.woff -L
+curl -o ./fonts/aileron-ultra-light.ttf https://staging.tiny.cloud/fonts/aileron-ultra-light.ttf -L

5f7ef92d6863502fa2ebbe53fbf1d6b101903a64
grade-zero <b.cream87@gmail.com>
2019-07-16 16:58:49 +1000

    WEBSITE-1692 - Cart and checkout buttons in nav

---
+++ _includes/js/store.js
@@ -0,0 +1,37 @@
+(function () {
+
+  if (typeof window.fetch === "undefined" ||
+      typeof window.$ === "undefined"
+  ) {
+    return;
+  }
+
+  var ENDPOINT = isStagingEnvironment()
+      ? "https://apps.staging.tiny.cloud/tiny/order/"
+      : "https://apps.tiny.cloud/tiny/order/";
+
+  function fetchCart() {
+    fetch(ENDPOINT, {
+      credentials: "include",
+      method: "GET",
+      mode: "cors"
+    }).then(function (response) {
+      return response.json()
+    }).then(function (response) {
+      if (response.itemCount > 0) {
+        showStoreButtons();
+      }
+    });
+  }
+
+  function isStagingEnvironment() {
+    return window.location.hostname.indexOf("staging") >= 0 ||
+        window.location.hostname.indexOf("localhost") >= 0;
+  }
+
+  function showStoreButtons() {
+    $(".tiny-news-store").css("visibility", "unset");
+  }
+
+  $(document).ready(fetchCart);
+})();

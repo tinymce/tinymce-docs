520b87a0f6790c3de46a2dc00a094b58e4c45699
grade-zero <b.cream87@gmail.com>
2019-07-16 16:27:42 +1000

    WEBSITE-1703 - xhr test

--- _includes/js/store.js
+++ _includes/js/store.js
@@ -31,7 +31,7 @@
     xhr.send();
     xhr.onload = function() {
       if (xhr.status != 200) {
-        console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+        console.log("Error", xhr.status, xhr.statusText);
       } else {
         console.log("response", xhr.response, JSON.parse(xhr.response));
         if (JSON.parse(xhr.response).itemCount > 0) {

3b11c7bacda71e0e7a634b4985d60db1236ea0cb
Tyler Kelly <tyler.kelly@tiny.cloud>
2019-07-30 16:56:53 +1000

    Added missing plugins to full-featured demo. Appended list of excluded plugins.

--- _includes/codepens/full-featured/index.js
+++ _includes/codepens/full-featured/index.js
@@ -1,3 +1,4 @@
+
 var fakeServer = (function () {
   /* Use tinymce's Promise shim */
   var Promise = tinymce.util.Promise;
@@ -217,16 +218,28 @@ var mentions_select = function (mention, success) {
 
 tinymce.init({
   selector: 'textarea#full-featured',
-  plugins: 'print preview fullpage powerpaste tinydrive searchreplace autolink directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount tinymcespellchecker a11ychecker imagetools textpattern help formatpainter permanentpen pageembed tinycomments mentions linkchecker',
+  plugins: 'print preview fullpage powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons',
   imagetools_cors_hosts: ['picsum.photos'],
   tinydrive_token_provider: function (success, failure) {
     success({ token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJqb2huZG9lIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.Ks_BdfH4CWilyzLNk8S2gDARFhuxIauLa8PwhdEQhEo' });
   },
-  tinydrive_demo_files_url: '{{ site.baseurl }}/demo/tiny-drive-demo/demo_files.json',
+  tinydrive_demo_files_url: '/demo/tiny-drive-demo/demo_files.json',
   tinydrive_dropbox_app_key: 'jee1s9eykoh752j',
   tinydrive_google_drive_key: 'AIzaSyAsVRuCBc-BLQ1xNKtnLHB3AeoK-xmOrTc',
   tinydrive_google_drive_client_id: '748627179519-p9vv3va1mppc66fikai92b3ru73mpukf.apps.googleusercontent.com',
-  toolbar: 'formatselect | bold italic strikethrough forecolor backcolor permanentpen formatpainter | insertfile image link media pageembed | alignleft aligncenter alignright alignjustify  | numlist bullist outdent indent | removeformat | addcomment',
+  menu: {
+    tc: {
+      title: 'TinyComments',
+      items: 'addcomment showcomments deleteallconversations'
+    }
+  },
+  menubar: 'file edit view insert format tools table tc help',
+  toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
+  autosave_ask_before_unload: true,
+  autosave_interval: "30s",
+  autosave_prefix: "{path}{query}-{id}-",
+  autosave_restore_when_empty: false,
+  autosave_retention: "2m",
   image_advtab: true,
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
@@ -263,17 +276,22 @@ tinymce.init({
     }
   },
   templates: [
-    { title: 'Some title 1', description: 'Some desc 1', content: 'My content' },
-    { title: 'Some title 2', description: 'Some desc 2', content: '<div class="mceTmpl"><span class="cdate">cdate</span><span class="mdate">mdate</span>My content2</div>' }
+        { title: 'New Table', description: 'creates a new table', content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>' },
+    { title: 'Starting my story', description: 'A cure for writers block', content: 'Once upon a time...' },
+    { title: 'New list with dates', description: 'New List with dates', content: '<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>' }
   ],
-  template_cdate_format: '[CDATE: %m/%d/%Y : %H:%M:%S]',
-  template_mdate_format: '[MDATE: %m/%d/%Y : %H:%M:%S]',
+  template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
+  template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
+  height: 600,
   image_caption: true,
+  quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
+  noneditable_noneditable_class: "mceNonEditable",
+  toolbar_drawer: 'sliding',
   spellchecker_dialog: true,
   spellchecker_whitelist: ['Ephox', 'Moxiecode'],
   tinycomments_mode: 'embedded',
   content_style: ".mymention{ color: green; }",
-
+  contextmenu: "link image imagetools table configurepermanentpen",
   mentions_selector: '.mymention',
   mentions_fetch: mentions_fetch,
   mentions_menu_hover: mentions_menu_hover,
--- demo/full-featured.md
+++ demo/full-featured.md
@@ -7,6 +7,16 @@ description: This example displays all plugins that come with TinyMCE Cloud's pr
 keywords: example demo custom wysiwyg full-power full-featured
 ---
 
-This example demonstrates the available TinyMCE plugins, including plugins available on TinyMCE Cloud premium subscriptions (excluding [MoxieManager](/plugins/moxiemanager)). Please note, it includes [premium plugins](/pricing/#demo-enterprise).
+This example includes most of the available TinyMCE plugins, including plugins available on TinyMCE Cloud premium subscriptions. Please note, it includes [premium plugins](/pricing/#demo-enterprise).
 
-{% include codepen.html id="full-featured" height="750" %}
+{% include codepen.html id="full-featured" %}
+
+The following plugins are excluded from this example:
+* [MoxieManager (Premium Plugin)](/plugins/moxiemanager) — [**Tiny Drive**](/plugins/drive) included instead.
+* [Autoresize](/plugins/autoresize) — Resizes the editor to fit the content.
+* [BBCode](/plugins/bbcode) — Changes the markup used for the content.
+* [Code](/plugins/code) — [**Advanced Code Editor**](/plugins/advcode) included instead.
+* [Full Page](/plugins/fullpage) — Used for modifying HTML `<head>` elements.
+* [Paste](/plugins/paste) — [**PowerPaste**](/plugins/powerpaste) included instead.
+* [Spellchecker](/plugins/spellchecker) — [**Spell Checker Pro**](/plugins/tinymcespellchecker) included instead.
+* [Tab Focus](/plugins/tabfocus) — Changes the behavior of the TAB-key within the editor.

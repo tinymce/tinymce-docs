c60e724a15cc991d3247da556b79729c02c79fb2
Tyler Kelly <tyler.kelly@outlook.com>
2019-10-31 10:03:19 +1000

    Revert "Revert "Advanced Tables documentation""

---
+++ _includes/codepens/advtable-plugin/index.js
@@ -0,0 +1,6 @@
+tinymce.init({
+  selector: 'textarea#advtable-plugin',
+  height: '900px',
+  plugins: 'table code advtable fullscreen',
+  toolbar: 'undo redo | formatselect | bold italic | alignleft aligncenter alignright alignjustify | indent outdent | table tableinsertdialog tablecellprops tableprops | fullscreen'
+});

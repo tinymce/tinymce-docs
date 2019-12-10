be9c330cb1666857f3d429453f8a8419212eb340
Tyler Kelly <tyler.kelly@tiny.cloud>
2019-12-11 09:23:26 +1000

    fixing basic codepen formatting

--- _includes/codepens/basic-example/index.js
+++ _includes/codepens/basic-example/index.js
@@ -12,7 +12,7 @@ tinymce.init({
   ' alignright alignjustify | bullist numlist outdent indent |' +
   ' removeformat | help',
   content_css: [
-  '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-  '//www.tiny.cloud/css/codepen.min.css'
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tiny.cloud/css/codepen.min.css'
   ]
 });

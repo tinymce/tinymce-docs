1398bdda3c81a5ceef78dd9cb3c7229e14a3dbb1
Tyler Kelly <tyler.kelly@tiny.cloud>
2020-02-20 13:41:06 +1000

    feat: checklist menu item for 5.2

--- _includes/codepens/checklist/index.js
+++ _includes/codepens/checklist/index.js
@@ -2,5 +2,9 @@ tinymce.init({
   selector: "textarea#checklist",
   plugins: "lists checklist",
   toolbar: "checklist",
-  height: "600px",
+  height: 200,
+  width: 350,
+  menu: {
+	  format: { title: 'Format', items: 'checklist | bold italic underline strikethrough superscript subscript codeformat | formats blockformats fontformats fontsizes align | forecolor backcolor | removeformat' }
+  }
 });

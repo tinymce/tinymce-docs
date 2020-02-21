cae6f293040fbe2316c5ccb0edec021b1ec1c162
Tyler Kelly <tyler.kelly@tiny.cloud>
2020-02-21 12:33:56 +1000

    feat: remove spellchecker_on_load from demo

--- _includes/codepens/spellcheckerpro/index.js
+++ _includes/codepens/spellcheckerpro/index.js
@@ -3,7 +3,6 @@ tinymce.init({
   plugins: 'code tinymcespellchecker link',
   toolbar: 'spellchecker language spellcheckdialog',
   height: 500,
-  spellchecker_on_load: true,
   spellchecker_language: 'en',
   content_css: [
     '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',

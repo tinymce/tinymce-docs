68f136441544da4528e2c27ed31b4401ff581b34
Tyler Kelly <tyler.kelly@tiny.cloud>
2020-09-29 10:30:46 +1000

    DOC-654: replacing forum links with stackoverflow (#1686)
    * DOC-654: replacing forum links with stackoverflow
    
    * DOC-654: Remove mceNonEditable class from selected links

--- _scripts/check-links-staging.sh
+++ _scripts/check-links-staging.sh
@@ -4,4 +4,4 @@
 # https://www.notion.so/tinycloud/Check-for-broken-links-4876019acee44cc88c5082e447c85519
 
 mkdir -p ./_report/report
-linkchecker -r 2 -t 10 https://staging.tiny.cloud/docs/ --check-extern --output=html --ignore-url=https://community.tiny.cloud/.* > _report/report/index.html || true
+linkchecker -r 2 -t 10 https://staging.tiny.cloud/docs/ --check-extern --output=html --ignore-url=https://github.com/tinymce/tinymce-docs/tree/* --ignore-url=https://github.com/tinymce/tinymce-docs/issues/* > _report/report/index.html || true

aa5cc8114451daba79aecb57e050a8eda298f592
tylerkelly13 <tyler.kelly@tiny.cloud>
2020-12-04 11:32:56 +1000

    #1785: Add example.js to url dialog demo to remove codepen link

---
+++ _includes/live-demos/url-dialog/example.js
@@ -0,0 +1,19 @@
+tinymce.init({
+  selector: 'textarea.urldialog',
+  toolbar: 'urldialog',
+  height: 300,
+  setup: function (editor) {
+    editor.ui.registry.addButton('urldialog', {
+      icon: 'code-sample',
+      onAction: function () {
+        editor.windowManager.openUrl({
+          title: 'URL Dialog Demo',
+          url: '../../demo/url-dialog-demo/external-page.html',
+          height: 640,
+          width: 640
+        });
+      }
+    })
+  },
+  content_style: {{site.liveDemoIframeCSSStyles}}
+});

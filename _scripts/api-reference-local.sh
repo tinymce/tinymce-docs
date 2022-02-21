cc02bedf19786415b379f63df970809bb061c4d7
EkimChau <mike.chau@tiny.cloud>
2022-02-21 13:54:45 +1000

    Feature/6.0 api docs (#2225)
    * clean-up dev script
    
    * cleanup - now depends on the new moxiedoc antora feature branch to generate api docs in the .adoc format.  The generated files will now live in modules/root after running the api-reference script
    
    * DOC-1541: wire in moxiedoc generated content, wire in new api navigation as include (no need to append).  Navigation and api pages should render
    
    * DOC-1541: fixed antora crashing problem, static content and adoc content thats really out of sync can crash antora, we clear both caches when we build moxiedocs
    
    * DOC-1541: cleanup moxiedoc tmp folders, move navigation to same location as the main navigation file
    
    * DOC-1541: Simplify install of docs, yarn install, yarn build and yarn serve is all you need to develop with TinyMCE-Docs
    
    * DOC-1541: Docs version 2.0 and cleanup
    
    * DOC-1541: update yarn.lock with moxiedoc dep
    
    * DOC-1541: Improve README, live demos are still todo
    
    * DOC-1541: load development branch of TinyMCE to generate moxiedoc api, TODO change to 6.0 on release
    
    * DOC-1541: fixed wrong file path case to be upper cased
    
    * Use the correct moxiedoc package
    
    * DOC-1541: Update .gitignore to ignore API docs and other tmp files
    
    * DOC-1541: Update to the latest moxiedoc version
    
    * DOC-1541: Update to the latest moxiedoc version
    
    * DOC-1541: Update to the latest moxiedoc version
    
    * DOC-1541: Update to the latest moxiedoc version
    
    * DOC-1541: Update to the latest moxiedoc version
    
    Co-authored-by: tylerkelly13 <tyler.kelly@tiny.cloud>
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>

--- _scripts/api-reference-local.sh
+++ _scripts/api-reference-local.sh
@@ -14,10 +14,22 @@ echo -e "\n > importing data files for tinymce api reference: local from $1\n"
 
 rm -rf "$API_TMPDIR"
 mkdir "$API_TMPDIR"
-moxiedoc "$1/modules/tinymce/src/core/main/ts" -t tinymcenext -o "$API_TMPDIR/tinymce-api-reference.zip"
+npx moxiedoc "$1/modules/tinymce/src/core/main/ts" -t antora -o "$API_TMPDIR/tinymce-api-reference.zip"
 unzip -o "$API_TMPDIR/tinymce-api-reference.zip"
-rm -rf apis/
-mv api/ apis
-sed -i "s;^- url: api;- url: apis;" _data/nav_api.yml
 
-echo ""
+# remove old api adoc pages
+rm -rf modules/ROOT/pages/apis/
+
+# removed old static api html pages (clear cache), it can corrupt the build
+rm -rf build/site/_/tinymce/6.0/apis/
+
+# move newly generated adoc pages, antora will then generate new static html pages
+mv _data/antora modules/ROOT/pages/apis
+
+# move api navigation
+mv _data/moxiedoc_nav.adoc modules/ROOT/moxiedoc_nav.adoc
+
+# cleanup moxiedoc tmp _data folder
+rm -rf _data
+
+echo "donesky - using local build of tinymce"
--- _scripts/api-reference.sh
+++ _scripts/api-reference.sh
@@ -2,21 +2,33 @@
 
 set -e
 
-API_VERSION="$(cat .api-version)"
+API_VERSION="develop"
 TARBALL_URL="https://codeload.github.com/tinymce/tinymce/tar.gz/$API_VERSION"
 API_TMPDIR="/tmp/tinymce-$API_VERSION"
 
 echo -e "\n > importing data files for tinymce api reference: $API_VERSION\n"
 
-rm -f _data/nav_api.json
-rm -rf "$API_TMPDIR"
+rm -rf _data
 
+rm -rf "$API_TMPDIR"
 mkdir "$API_TMPDIR"
 curl -s "$TARBALL_URL" | tar xzf - -C "$API_TMPDIR" --strip-components 1
-moxiedoc "$API_TMPDIR/modules/tinymce/src/core/main/ts" -t tinymcenext -o "$API_TMPDIR/tinymce-api-reference.zip"
+npx moxiedoc "$API_TMPDIR/modules/tinymce/src/core/main/ts" -t antora -o "$API_TMPDIR/tinymce-api-reference.zip"
 unzip -o "$API_TMPDIR/tinymce-api-reference.zip"
-rm -rf apis/
-mv api/ apis
-sed -i "s;^- url: api;- url: apis;" _data/nav_api.yml
 
-echo ""
+# remove old api adoc pages
+rm -rf modules/ROOT/pages/apis/
+
+# removed old static api html pages (clear cache), it can corrupt the build
+rm -rf build/site/_/tinymce/6.0/apis/
+
+# move newly generated adoc pages, antora will then generate new static html pages
+mv _data/antora modules/ROOT/pages/apis
+
+# move api navigation
+mv _data/moxiedoc_nav.adoc modules/ROOT/moxiedoc_nav.adoc
+
+# cleanup moxiedoc tmp _data folder
+rm -rf _data
+
+echo "donesky"

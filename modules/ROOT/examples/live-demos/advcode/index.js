8a6291b4ce193307250fb614da9af6c6b17af208
Luke Trouton <luke.trouton@tiny.cloud>
2022-03-24 12:51:25 +1000

    DOC-1576: Fix accidental new lines

--- modules/ROOT/examples/live-demos/advcode/index.js
+++ modules/ROOT/examples/live-demos/advcode/index.js
@@ -2,7 +2,6 @@ tinymce.init({
   selector: 'textarea.advcodedemo',
   plugins: 'advcode',
   toolbar: 'code',
-
   height: 600,
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });
@@ -11,7 +10,6 @@ tinymce.init({
   selector: 'textarea.codedemo',
   plugins: 'code',
   toolbar: 'code',
-
   height: 600,
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });

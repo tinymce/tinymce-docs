39b1b090b5b78a5129ef65d138beedc0f6fd64ad
Lee Newson <lee.newson@tiny.cloud>
2022-03-26 17:39:25 +1000

    DOC-1616: Make the live-demo extension work fully with Antora

--- extensions/live-demo.js
+++ extensions/live-demo.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const { Liquid } = require('liquidjs');
 
 /**
@@ -65,6 +64,11 @@ const defaultTabs = [
   }
 ];
 
+const loadDemoResource = (catalog, ctx, filePath) => {
+  const demoCss = catalog.resolveResource(`live-demos/${filePath}`, ctx, 'example', [ 'example' ]);
+  return demoCss ? demoCss.contents.toString() : undefined;
+};
+
 const getDemoTitle = (type) => {
   switch (type) {
     case 'tinydrive':
@@ -85,7 +89,15 @@ const getScript = (type, docAttrs) => {
   }
 };
 
-const getTabs = (type, contentData, initialTab) => {
+const getDemoCss = (catalog, type, ctx) => {
+  if (type === 'tinydrive') {
+    return loadDemoResource(catalog, ctx, 'tinydrive.css');
+  } else {
+    return '';
+  }
+};
+
+const getTabs = (type, contentData) => {
   const tabs = defaultTabs.filter((d) => {
     return d.name === 'run' || contentData.hasOwnProperty(d.name);
   }).map((d) => ({
@@ -109,15 +121,15 @@ const loadContent = (engine, catalog, id, docAttrs) => {
     module: docAttrs['page-module'],
     component: docAttrs['page-component-name'],
     version: docAttrs['page-component-version'],
-  }
+  };
 
   Object.entries(validContent).forEach(([type, file]) => {
     const hasKey = 'has' + type[0].toUpperCase() + type.slice(1);
 
     // If the file exists, then render the content
-    const catalogFile = catalog.resolveResource(`live-demos/${id}/${file}`, ctx, 'example', [ 'example' ]);
+    const catalogFile = loadDemoResource(catalog, ctx, `${id}/${file}`);
     if (catalogFile !== undefined) {
-      data[type] = engine.renderFileSync(catalogFile.path, {
+      data[type] = engine.parseAndRenderSync(catalogFile, {
         baseurl: `${docAttrs['site-url']}/${ctx.component}/${ctx.version}`,
         baseimagesurl: `${docAttrs['site-url']}/${ctx.component}/${ctx.version}/${docAttrs['imagesdir']}`,
         ...docAttrs
@@ -158,11 +170,17 @@ module.exports.register = (registry, context) => {
       const scriptUrl = attrs.script_url_override || getScript(type, docAttrs);
 
       // Render the template
-      const rootLiveDemoDir = path.join(docAttrs['page-origin-start-path'], 'modules', 'ROOT', 'examples', 'live-demos');
-      const renderedContent = engine.renderFileSync('live-demo.adoc.liquid', {
+      const rootCtx = {
+        module: 'ROOT',
+        component: docAttrs['page-component-name'],
+        version: docAttrs['page-component-version'],
+      };
+      const liveDemoFile = loadDemoResource(catalog, rootCtx, 'live-demo.adoc.liquid');
+      const renderedContent = engine.parseAndRenderSync(liveDemoFile, {
         liveDemo: {
           ...attrs,
           type: type,
+          css: getDemoCss(catalog, type, rootCtx),
           id: target,
           content: contentData,
           initialTab: initialTab,
@@ -170,9 +188,9 @@ module.exports.register = (registry, context) => {
             include: scriptsLoaded[scriptUrl] !== true,
             url: scriptUrl,
           },
-          tabs: getTabs(type, contentData, initialTab)
+          tabs: getTabs(type, contentData)
         }
-      }, { root: rootLiveDemoDir });
+      });
       scriptsLoaded[scriptUrl] = true;
 
       // Parse the content using AsciiDoctor

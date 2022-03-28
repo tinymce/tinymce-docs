0b0e72bebde5e6763d2cc53a1b46de2bec644a45
Ben Tran <ben.tran@tiny.cloud>
2022-03-28 11:49:24 +1030

    DOC-1572: Corrected the code of callback function

--- modules/ROOT/examples/live-demos/local-upload/index.js
+++ modules/ROOT/examples/live-demos/local-upload/index.js
@@ -7,11 +7,9 @@ tinymce.init({
   images_upload_url: 'postAcceptor.php',
 
   /* we override default upload handler to simulate successful upload*/
-  images_upload_handler: (blobInfo) => {
-    return new Promise((resolve) => {
+  images_upload_handler: (blobInfo) => new Promise((resolve) => {
       /* no matter what you upload, we will turn it into TinyMCE logo :)*/
-      resolve('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png');
-    });
-  },
+      setTimeout(() => resolve('http://moxiecode.cachefly.net/tinymce/v9/images/logo.png'), 2000);
+  }),
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });

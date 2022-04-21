49f18ac0c148793d6f16b9eef2b1da78d302246d
Lee Newson <lee.newson@tiny.cloud>
2022-04-21 17:38:57 +1000

    DOC-1651: Various minor docs fixes

--- modules/ROOT/examples/live-demos/basic-example/index.js
+++ modules/ROOT/examples/live-demos/basic-example/index.js
@@ -1,11 +1,10 @@
 tinymce.init({
   selector: 'textarea#basic-example',
   height: 500,
-  menubar: false,
   plugins: [
     'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
     'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-    'insertdatetime', 'media', 'table', 'code', 'help', 'wordcount'
+    'insertdatetime', 'media', 'table', 'help', 'wordcount'
   ],
   toolbar: 'undo redo | blocks | ' +
   'bold italic backcolor | alignleft aligncenter ' +

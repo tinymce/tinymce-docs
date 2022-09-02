60553647c6c9e3520b88d88288d6766ccef32b5b
MitchCTiny <mitchell.crompton@tiny.cloud>
2022-09-02 16:39:56 +1000

    DOC-1753: Added Spelling Autocorrect docs pages

---
+++ modules/ROOT/examples/live-demos/autocorrect/index.js
@@ -0,0 +1,10 @@
+tinymce.init({
+    selector: 'textarea#autocorrect',
+    plugins: 'autocorrect',
+    menu: {
+      autocorrect: { title: 'Autocorrect', items: 'autocorrect capitalization' }
+    },
+    menubar: 'autocorrect',
+	autocorrect_autocorrect: true,
+	autocorrect_capitalize: true
+});

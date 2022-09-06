885c489756f24042b35ca973dbafe9b1c12b464a
MitchCTiny <mitchell.crompton@tiny.cloud>
2022-09-06 10:33:24 +1000

    DOC-1753: Fix issues from review

--- modules/ROOT/examples/live-demos/autocorrect/index.js
+++ modules/ROOT/examples/live-demos/autocorrect/index.js
@@ -1,10 +1,6 @@
 tinymce.init({
     selector: 'textarea#autocorrect',
     plugins: 'autocorrect',
-    menu: {
-      autocorrect: { title: 'Autocorrect', items: 'autocorrect capitalization' }
-    },
-    menubar: 'autocorrect',
     autocorrect_autocorrect: true,
     autocorrect_capitalize: true
 });

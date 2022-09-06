6b1d23f128db517504f9b47ba67aa6f76d120ea0
tiny-ben-tran <ben.tran@tiny.cloud>
2022-09-07 09:03:00 +0930

    Update modules/ROOT/examples/live-demos/full-featured/example.js
    Co-authored-by: Lee Newson <lee.newson@tiny.cloud>

--- modules/ROOT/examples/live-demos/full-featured/example.js
+++ modules/ROOT/examples/live-demos/full-featured/example.js
@@ -69,7 +69,5 @@ tinymce.init({
   mentions_menu_hover: mentions_menu_hover,
   mentions_menu_complete: mentions_menu_complete,
   mentions_select: mentions_select,
-  mentions_item_type: 'profile',
-  autocorrect_autocorrect: true,
-  autocorrect_capitalize: true,
+  mentions_item_type: 'profile'
 });

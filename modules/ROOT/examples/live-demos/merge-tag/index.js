6688433985977257902661ad0d5bead1921e09a7
vpyshnenko <viktor.pyshnenko@tiny.cloud>
2022-09-08 12:02:46 +0300

    TINY-9138: merge tags demo

--- modules/ROOT/examples/live-demos/merge-tag/index.js
+++ modules/ROOT/examples/live-demos/merge-tag/index.js
@@ -1,12 +1,12 @@
 tinymce.init({
-    selector: "textarea",
-    plugins: "powerpaste a11ychecker linkchecker wordcount table advtable editimage autosave advlist anchor advcode image link lists media mediaembed searchreplace visualblocks template mergetags",
-    toolbar: "mergetags | styles | bold italic underline | link | align bullist numlist",
-	
-	mergetags_list: [
-    
-	{
-      title: 'Proposal',
+  selector: "textarea#mergetags",
+  plugins: "powerpaste a11ychecker linkchecker wordcount table advtable editimage autosave advlist anchor advcode image link lists media mediaembed searchreplace visualblocks template mergetags",
+  toolbar: "mergetags | undo redo | styles | bold italic underline | link | align bullist numlist",
+  mergetags_prefix: '{*',
+  mergetags_suffix: '*}',
+  mergetags_list: [
+    {
+      title: 'Client',
       menu: [
         {
           value: 'Client.LastCallDate',
@@ -15,19 +15,25 @@ tinymce.init({
         {
           value: 'Client.Name',
           title: 'Client name'
-        },
-        {
-          value: 'Consultant',
-          title: 'Consultant'
-        },
+        }
+      ]
+    },
+    {
+      title: 'Proposal',
+      menu: [
         {
           value: 'Proposal.SubmissionDate',
           title: 'Submission date'
-        },
-        {
-          value: 'Salutation',
-          title: 'Salutation'
         }
       ]
-    }]
+    },
+    {
+      value: 'Consultant',
+      title: 'Consultant'
+    },
+    {
+      value: 'Salutation',
+      title: 'Salutation'
+    }
+  ]
 });

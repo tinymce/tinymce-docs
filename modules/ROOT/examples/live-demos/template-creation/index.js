023bc708e03bf3cda07e17cbd61f725b2b230494
bforte <brian.forte@tiny.cloud>
2023-06-07 14:21:16 +1000

    DOC-2069: /template-creation/index.html & index.js
    Place-holder code and material placed in each file.

---
+++ modules/ROOT/examples/live-demos/template-creation/index.js
@@ -0,0 +1,15 @@
+tinymce.init({
+  selector: 'textarea#template-creation',
+  height: 1200,
+  plugins: [
+    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+    'insertdatetime', 'media', 'table', 'help', 'wordcount'
+  ],
+  toolbar: 'undo redo | blocks | ' +
+  'bold italic backcolor | alignleft aligncenter ' +
+  'alignright alignjustify | bullist numlist outdent indent | ' +
+  'removeformat | help',
+  editable_root: false,
+  newdocument_content: '<div contenteditable="true">Editable content</div>'
+});

821a23ac9a47d026e03c7fdc32721b730f9b1368
brian-forte <brian.forte@tiny.cloud>
2023-09-11 21:46:37 +1000

    Update modules/ROOT/examples/live-demos/advtemplate-insertionpoint/index.js
    Code presentation edits.
    
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>

--- modules/ROOT/examples/live-demos/advtemplate-insertionpoint/index.js
+++ modules/ROOT/examples/live-demos/advtemplate-insertionpoint/index.js
@@ -2,38 +2,34 @@ tinymce.init({
   selector: "textarea#advanced-template-insertionpoint",
   plugins: [ "advtemplate", "mergetags", ],
   toolbar: "inserttemplate | mergetags",
-advtemplate_templates:
-[
-  {
-  title: 'Without an insertion point marker',
-  items:
-    [
-      {
-        title: 'Name entry prompt',
-        content: '<p><strong>Enter your name:</strong></p><p><em>Include both your given and family names, in your preferred order.</em></p>'
-      },
-      {
-        title: 'Letter outline',
-        content: '<p>{{Current.Date}}</p><p>{{Honorific}} {{Person.Name.Last}},</p><p></p><p>&nbsp;</p><p>Regards,</p><p>{{staff.digital.signature}}</p><p>{{Staff.Name}}</p><p>{{Staff.Email}}</p>'
-      },
-
-    ],
-  },
-  {
-  title: 'With an insertion point marker',
-  items:
-    [
-      {
-        title: 'Name entry prompt',
-        content: '<p><strong>Enter your name:</strong>{{mce-cursor}}</p><p><em>Include both your given and family names, in your preferred order.</em></p>'
-      },
-      {
-        title: 'Letter outline',
-        content: '<p>{{Current.Date}}</p><p>{{Honorific}} {{Person.Name.Last}},</p><p>{{mce-cursor}}&nbsp;</p><p>Regards,</p><p>{{staff.digital.signature}}</p><p>{{Staff.Name}}</p><p>{{Staff.Email}}</p>'        
-      },
-    ],
-  },
-],
+  advtemplate_templates: [
+    {
+      title: 'Without an insertion point marker',
+      items: [
+        {
+          title: 'Name entry prompt',
+          content: '<p><strong>Enter your name:</strong></p><p><em>Include both your given and family names, in your preferred order.</em></p>'
+        },
+        {
+          title: 'Letter outline',
+          content: '<p>{{Current.Date}}</p><p>{{Honorific}} {{Person.Name.Last}},</p><p></p><p>&nbsp;</p><p>Regards,</p><p>{{staff.digital.signature}}</p><p>{{Staff.Name}}</p><p>{{Staff.Email}}</p>'
+        },
+      ],
+    },
+    {
+      title: 'With an insertion point marker',
+      items: [
+        {
+          title: 'Name entry prompt',
+          content: '<p><strong>Enter your name:</strong>{{mce-cursor}}</p><p><em>Include both your given and family names, in your preferred order.</em></p>'
+        },
+        {
+          title: 'Letter outline',
+          content: '<p>{{Current.Date}}</p><p>{{Honorific}} {{Person.Name.Last}},</p><p>{{mce-cursor}}&nbsp;</p><p>Regards,</p><p>{{staff.digital.signature}}</p><p>{{Staff.Name}}</p><p>{{Staff.Email}}</p>'
+        },
+      ],
+    },
+  ],
   mergetags_list: [
     {
       value: 'Current.Date',
@@ -42,9 +38,9 @@ advtemplate_templates:
     {
       value: 'Current.Time',
     },
-	{
-	  value: 'Honorific',
-	},
+    {
+      value: 'Honorific',
+    },
     {
       title: 'Person',
       menu: [
@@ -60,14 +56,14 @@ advtemplate_templates:
           value: 'Customer.Name.Full',
           title: 'customer full name'
         },
-		{
-		  value: 'Staff.Name.Full',
-		  title: 'staff full name'
-		},
-		{
-		  value: 'Staff.Digital.Signature',
-		  title: 'staff digital signaure'
-		},
+        {
+          value: 'Staff.Name.Full',
+          title: 'staff full name'
+        },
+        {
+          value: 'Staff.Digital.Signature',
+          title: 'staff digital signaure'
+        },
         {
           title: 'Email',
           menu: [

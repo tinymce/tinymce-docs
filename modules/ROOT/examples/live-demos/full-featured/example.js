434d2d92f283e5f95e06457e55c8cfcdb7b72bef
bforte <brian.forte@tiny.cloud>
2023-09-26 11:45:40 +1000

    DOC-2075: live-demos/full-featured/example.js|index.js
    Temporary configuration values were added to `spellchecker_ignore_list` in the *Full-featured-demo with Premium plugins* to workaround a bug in the interaction between the `tinymcespellchecker` and `typography` Premium plugins.
    
    That bug will be fixed as part of TinyMCE 6.8.
    
    So, the temporary configuration values were removed.

--- modules/ROOT/examples/live-demos/full-featured/example.js
+++ modules/ROOT/examples/live-demos/full-featured/example.js
@@ -100,7 +100,7 @@ tinymce.init({
   quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
   noneditable_class: 'mceNonEditable',
   toolbar_mode: 'sliding',
-  spellchecker_ignore_list: ['Ephox', 'Moxiecode', 'tinymce', 'TinyMCE', 'tm', 'TM'], // ‘tm’ and ‘TM’ are temporary spellchecker bug workarounds
+  spellchecker_ignore_list: ['Ephox', 'Moxiecode', 'tinymce', 'TinyMCE'],
   tinycomments_mode: 'embedded',
   content_style: '.mymention{ color: gray; }',
   contextmenu: 'link image editimage table configurepermanentpen',

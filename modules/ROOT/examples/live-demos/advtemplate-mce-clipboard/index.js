ea13ba0f7fdf2ee629afdc5e6a18320f26c906bd
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2023-11-22 21:02:12 +1000

    DOC-2215: updated demo files to remove template categories.

--- modules/ROOT/examples/live-demos/advtemplate-mce-clipboard/index.js
+++ modules/ROOT/examples/live-demos/advtemplate-mce-clipboard/index.js
@@ -5,21 +5,11 @@ tinymce.init({
   advtemplate_templates: [
     {
       title: 'Without an mce-clipboard marker',
-      items: [
-        {
-          title: 'Paste your clipboard',
-          content: '<p><strong>Paste your content here:</strong></p><p><em> using mce-clipboard new advanced template paste feature.</em></p>'
-        },
-      ],
+      content: '<p>Your clipboard content will <strong>not be inserted</strong> after this marker</p><p><em> or before this marker when using mce-clipboard new advanced template paste feature.</em></p>'
     },
     {
       title: 'With an mce-clipboard marker',
-      items: [
-        {
-          title: 'Paste your clipboard',
-          content: '<p><strong>Paste your content here:</strong>{{mce-clipboard}}</p><p><em> using mce-clipboard new advanced template paste feature.</em></p>'
-        },
-      ],
-    },
+      content: '<p>Your clipboard content will <strong>be inserted</strong> after this marker {{mce-clipboard}} </p><p><em> and, before this marker when using mce-clipboard new advanced template paste feature.</em></p>'
+    }
   ],
 });

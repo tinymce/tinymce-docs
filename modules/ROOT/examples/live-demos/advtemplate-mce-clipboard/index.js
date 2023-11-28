61ae77770f7e77fe9974328c9f2c782c1d3d5b13
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2023-11-28 20:15:32 +1000

    DOC-2215: correcting index.js backslash  for antora syntax.

--- modules/ROOT/examples/live-demos/advtemplate-mce-clipboard/index.js
+++ modules/ROOT/examples/live-demos/advtemplate-mce-clipboard/index.js
@@ -9,7 +9,7 @@ tinymce.init({
     },
     {
       title: 'With an mce-clipboard marker',
-      content: '<p>Hi {{mce-clipboard}}, Thank you for visiting this page. We truly appreciate and value your feedback and any feature requests you may have While you are here, take a moment to explore mce-cursor {{mce-clipboard}}, its another powerful tool designed for Advanced Templates</p>'
+      content: '<p>Hi {\{mce-clipboard\}}, Thank you for visiting this page. We truly appreciate and value your feedback and any feature requests you may have While you are here, take a moment to explore mce-cursor {\{mce-clipboard\}}, its another powerful tool designed for Advanced Templates</p>'
     }
   ],
 });

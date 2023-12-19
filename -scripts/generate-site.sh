1ad0951e1f8cf522bae8b927ea36d8b956d7a48a
MitchCTiny <mitchell.crompton@tiny.cloud>
2023-12-19 15:56:29 +1000

    DOC-2220: Testing generation

--- -scripts/generate-site.sh
+++ -scripts/generate-site.sh
@@ -7,9 +7,9 @@ BUILD_DIR="build"
 SITE_DIR="$BUILD_DIR/site/tinymce"
 
 SOURCE_VERSION="latest"
-SOURCE_DIR="$SITE_DIR/$SOURCE_VERSION/"
+SOURCE_DIR="$SITE_DIR/$SOURCE_VERSION"
 DEST_VERSION=6
-DEST_DIR="$SITE_DIR/$DEST_VERSION/"
+DEST_DIR="$SITE_DIR/$DEST_VERSION"
 
 echo -e "\nRemoving existing $SITE_DIR directory."
 rm -rf "$SITE_DIR"
@@ -19,7 +19,6 @@ antora "$PLAYBOOK"
 
 echo -e "\nCopying all $SOURCE_VERSION content to $DEST_VERSION directory."
 mkdir "$DEST_DIR"
-# cp -a "$SOURCE_DIR." "$DEST_DIR"
 
 cp -a "$SOURCE_DIR" "$DEST_DIR"
 

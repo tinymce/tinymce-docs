01304bb9b36a7c6ecdf2a3219af2c23e7b60f6b8
Farzad Hayatbakhsh <farzad.hayatbakhsh@tiny.cloud>
2024-02-02 17:14:28 +1000

    add demo outline for noneditable-regexp option

---
+++ modules/ROOT/examples/live-demos/noneditable-regexp/index.js
@@ -0,0 +1,15 @@
+tinymce.init({
+  selector: 'textarea#noneditable-regexp',
+  height: 500,
+  plugins: [
+    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
+    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
+    'insertdatetime', 'media', 'table', 'help', 'wordcount'
+  ],
+  toolbar: 'undo redo | blocks | ' +
+  'bold italic backcolor | alignleft aligncenter ' +
+  'alignright alignjustify | bullist numlist outdent indent | ' +
+  'removeformat | help',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+  noneditable_regexp: /apikey|\d{3}-\d{3}-\d{3}/g,
+});

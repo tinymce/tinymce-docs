9941fff514e2e9faa07fe61ef6454ef759ac4ee8
Farzad Hayatbakhsh <farzad.hayatbakhsh@tiny.cloud>
2024-02-05 12:58:53 +1000

    improve noneditable-regexp demo

--- modules/ROOT/examples/live-demos/noneditable-regexp/index.js
+++ modules/ROOT/examples/live-demos/noneditable-regexp/index.js
@@ -1,15 +1,10 @@
 tinymce.init({
   selector: 'textarea#noneditable-regexp',
   height: 500,
-  plugins: [
-    'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
-    'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
-    'insertdatetime', 'media', 'table', 'help', 'wordcount'
-  ],
-  toolbar: 'undo redo | blocks | ' +
-  'bold italic backcolor | alignleft aligncenter ' +
-  'alignright alignjustify | bullist numlist outdent indent | ' +
-  'removeformat | help',
-  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+  content_style: `
+  body { font-family:Helvetica,Arial,sans-serif; font-size:16px }
+  .green { border: 0.1rem solid green; border-radius: 0.8rem; padding: 0.2rem; }
+  .red * { border: 0.1rem solid red; border-radius: 0.8rem; padding: 0.2rem; }
+  `,
   noneditable_regexp: /apikey|\d{3}-\d{3}-\d{3}/g,
 });

b305f72a2a8905c5c203655d6d1ba106024e85b7
Farzad Hayatbakhsh <farzad.hayatbakhsh@tiny.cloud>
2024-02-08 16:24:08 +1000

    added getusers-api-alternative demo to 6.0 release notes and the 5 to 6 migration guide

---
+++ modules/ROOT/examples/live-demos/getusers-api-alternative/index.js
@@ -0,0 +1,54 @@
+const users = [
+  { id: 'user-1', name: 'John Doe' },
+  { id: 'user-2', name: 'Jane Doe' },
+  { id: 'user-3', name: 'Abraham Lincoln' }
+];
+
+const fetchMentions = (query, success) => {
+  const matches = users.filter((user) => user.name.toLowerCase().includes(query.term.toLowerCase()));
+  success(matches);
+};
+
+const insertedUsers = new Set();
+
+const mentionsInsert = (editor, user) => {
+  const span = editor.getDoc().createElement('span');
+  span.className = 'mention';
+  span.appendChild(editor.getDoc().createTextNode('@' + user.name));
+  insertedUsers.add(user);
+  return span;
+};
+
+const getUsers = (editor) => {
+  const spans = Array.from(editor.getBody().querySelectorAll('[data-mce-mentions-id]'));
+  const currentIds = spans.map((elm) => elm.getAttribute('data-mce-mentions-id'));
+  const currentUsers = Array.from(insertedUsers).filter((user) => currentIds.includes(user.id));
+
+  return currentUsers;
+};
+
+tinymce.init({
+  selector: "textarea#getusers-api-alternative",
+  plugins: "mentions code",
+  toolbar: "undo redo | styles | bold italic underline strikethrough | code",
+  mentions_fetch: fetchMentions,
+  mentions_menu_complete: mentionsInsert,
+
+  toolbar: 'getUsersButton',
+  setup: function (editor) {
+	editor.ui.registry.addButton('getUsersButton', {
+	  text: 'getUsers',
+	  onAction: function () {
+		// Get mentioned users
+		var mentionedUsers = getUsers(editor);
+    // Insert mentioned users as a bullet list
+		if (mentionedUsers.length > 0) {
+		  var userList = mentionedUsers.map(function (user) {
+			return '<li>' + user.name + '</li>';
+		  }).join('');
+		  editor.execCommand('mceInsertContent', false, '<p>Mentioned Users:</p><ul>' + userList + '</ul>');
+		}
+	  }
+	});
+  }
+});

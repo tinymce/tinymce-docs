c1c80ce73f92b0029f7422292e1ef7c141ef44cf
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2024-02-14 15:57:52 +1000

    DOC-2266: Add new documenation for ExportPDF plugin.

---
+++ modules/ROOT/examples/live-demos/exportpdf/index.js
@@ -0,0 +1,7 @@
+tinymce.init({
+  selector: 'textarea.exportpdf',
+  plugins: 'exportpdf',
+  toolbar: 'exportpdf',
+  height: 600,
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
+});

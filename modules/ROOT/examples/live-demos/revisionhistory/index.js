b4dc8ed095169d16405c27256a86f121b682e1e2
Ben Tran <ben.tran@tiny.cloud>
2024-03-13 15:41:53 +1030

    DOC-2248: Update demos

--- modules/ROOT/examples/live-demos/revisionhistory/index.js
+++ modules/ROOT/examples/live-demos/revisionhistory/index.js
@@ -1,6 +1,6 @@
 const revisions = [
   {
-    revisionId: '3',
+    revisionId: '4',
     createdAt: '2023-11-29T10:11:21.578Z',
     content: `
       <h2>The world&rsquo;s first Rich Text Editor in the cloud</h2>
@@ -10,7 +10,6 @@ const revisions = [
       <p>Here are some of our customer&rsquo;s most common use cases for TinyMCE:</p>
       <ol>
       <li>Content Management Systems (<em>e.g. WordPress, Umbraco</em>)</li>
-      <li>Learning Management Systems (<em>e.g. Blackboard</em>)</li>
       <li>Customer Relationship Management and marketing automation (<em>e.g. Marketo</em>)</li>
       <li>Email marketing (<em>e.g. Constant Contact</em>)</li>
       <li>Content creation in SaaS systems (<em>e.g. Eventbrite, Evernote, GoFundMe, Zendesk</em>)</li>
@@ -18,27 +17,28 @@ const revisions = [
     `,
   },
   {
-    revisionId: '2',
+    revisionId: '3',
     createdAt: '2023-11-25T08:30:21.578Z',
     content: `
       <h2>The world&rsquo;s first rich text editor in the cloud</h2>
       <p>&nbsp;</p>
       <div>Have you heard about Tiny Cloud? It&rsquo;s the first step in our journey to help you deliver great content creation experiences, no matter your level of expertise. 50,000 developers already agree. They get free access to our global CDN, image proxy services and auto updates to the TinyMCE editor. They&rsquo;re also ready for some exciting updates coming soon.</div>
+      <p>&nbsp;</p>
       <h3>An editor for every project</h3>
       <p>Here are some of our customer&rsquo;s most common use cases for TinyMCE:</p>
-      <ul>
+      <ol>
       <li>Content Management Systems (<em>e.g. WordPress, Umbraco</em>)</li>
       <li>Learning Management Systems (<em>e.g. Blackboard</em>)</li>
       <li>Customer Relationship Management and marketing automation (<em>e.g. Marketo</em>)</li>
       <li>Email marketing (<em>e.g. Constant Contact</em>)</li>
       <li>Content creation in SaaS systems (<em>e.g. Eventbrite, Evernote, GoFundMe, Zendesk</em>)</li>
-      </ul>
+      </ol>
       <p>&nbsp;</p>
       <p>And those use cases are just the start. TinyMCE is incredibly flexible, and with hundreds of APIs there&rsquo;s likely a solution for your editor project. If you haven&rsquo;t experienced Tiny Cloud, get started today. You&rsquo;ll even get a free trial of our premium plugins &ndash; no credit card required!</p>
     `,
   },
   {
-    revisionId: '1',
+    revisionId: '2',
     createdAt: '2023-11-24T22:26:21.578Z',
     content: `
       <h2>The world&rsquo;s first rich text editor in the cloud</h2>
@@ -59,11 +59,35 @@ const revisions = [
       <p>And those use cases are just the start. TinyMCE is incredibly flexible, and with hundreds of APIs there&rsquo;s likely a solution for your editor project. If you haven&rsquo;t experienced Tiny Cloud, get started today. You&rsquo;ll even get a free trial of our premium plugins &ndash; no credit card required!</p>
     `,
   },
+  {
+    revisionId: '1',
+    createdAt: '2023-11-23T20:26:21.578Z',
+    content: `
+      <!--
+        <h2>The world&rsquo;s first rich text editor in the cloud</h2>
+        <p>&nbsp;</p>
+        <div>Have you heard about Tiny Cloud? It&rsquo;s the first step in our journey to help you deliver great content creation experiences, no matter your level of expertise. 50,000 developers already agree. They get free access to our global CDN, image proxy services and auto updates to the TinyMCE editor. They&rsquo;re also ready for some exciting updates coming soon.</div>
+        <p>&nbsp;</p>
+        <p>One of these enhancements is <strong>Tiny Drive</strong>: imagine file management for TinyMCE, in the cloud, made super easy. Learn more at <a href="https://www.tiny.cloud/drive/">Tiny Cloud - Tiny Drive</a>, where you&rsquo;ll find a working demo and an opportunity to provide feedback to the product team.</p>
+        <h3>An editor for every project</h3>
+        <p>Here are some of our customer&rsquo;s most common use cases for TinyMCE:</p>
+        <ul>
+        <li>Content Management Systems (<em>e.g. WordPress, Umbraco</em>)</li>
+        <li>Learning Management Systems (<em>e.g. Blackboard</em>)</li>
+        <li>Customer Relationship Management and marketing automation (<em>e.g. Marketo</em>)</li>
+        <li>Email marketing (<em>e.g. Constant Contact</em>)</li>
+        <li>Content creation in SaaS systems (<em>e.g. Eventbrite, Evernote, GoFundMe, Zendesk</em>)</li>
+        </ul>
+        <p>&nbsp;</p>
+        <p>And those use cases are just the start. TinyMCE is incredibly flexible, and with hundreds of APIs there&rsquo;s likely a solution for your editor project. If you haven&rsquo;t experienced Tiny Cloud, get started today. You&rsquo;ll even get a free trial of our premium plugins &ndash; no credit card required!</p>
+      -->
+    `,
+  }
 ];
 
 const get_revisions = () => new Promise((resolve) => {
     setTimeout(() => {
-      resolve(revisions.sort((a, b) => a.createdAt - b.createdAt));
+      resolve(revisions.sort((a, b) => new Date(a.createdAt) < new Date(b.createdAt) ? -1 : 1).reverse());
     }, 1000);
   });
 
@@ -73,6 +97,6 @@ tinymce.init({
   height: 800,
   plugins: 'revisionhistory',
   toolbar: 'revisionhistory',
-	revisionhistory_fetch: get_revisions,
+  revisionhistory_fetch: get_revisions,
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });

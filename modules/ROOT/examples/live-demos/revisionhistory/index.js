4e5c343902b68a003b0e48eaf6c09a92f762de55
Farzad Hayatbakhsh <farzad.hayatbakhsh@tiny.cloud>
2024-06-14 11:54:52 +1000

    Update Revision History demo to include more plugins and toolbar buttons

--- modules/ROOT/examples/live-demos/revisionhistory/index.js
+++ modules/ROOT/examples/live-demos/revisionhistory/index.js
@@ -141,8 +141,8 @@ const revisionhistory_fetch_revision = (_editor, revision) =>
 tinymce.init({
   selector: 'textarea#revisionhistory',
   height: 800,
-  plugins: 'revisionhistory',
-  toolbar: 'revisionhistory',
+  plugins: 'revisionhistory advlist autolink lists link image charmap preview anchor searchreplace visualblocks code fullscreen insertdatetime media table help wordcount',
+  toolbar: 'revisionhistory | undo redo | blocks | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
   revisionhistory_fetch,
   revisionhistory_fetch_revision,

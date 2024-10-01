ccff2ceaac7f8c93b5ca026a833a6b6fdcf3dd85
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2024-10-01 22:14:31 +1000

    DOC-2524: New comments option: Comments can be made and read, but the editor is in a readonly state.

---
+++ modules/ROOT/examples/live-demos/comments-readonly-mode/index.js
@@ -0,0 +1,23 @@
+let editor;
+
+tinymce.init({
+  selector: "textarea#comment-readonly-ui-mode",
+  plugins: [
+    "tinycomments", "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
+    "help", "image", "insertdatetime", "link", "lists", "media",
+    "preview", "searchreplace", "table", "visualblocks",
+  ],
+  toolbar: "addcomment showcomments | undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  tinycomments_mode: 'embedded',
+  tinycomments_access: '',
+  setup: (ed) => {
+    editor = ed;  // Assign the editor instance globally
+  }
+});
+
+const button = document.getElementById('toggle-readonly-mode');
+
+button.addEventListener('click', () => {
+  const isReadonly = editor.mode.get() === 'readonly';
+ tinymce.get(0).mode.set(isReadonly ? 'design' : 'readonly');
+});

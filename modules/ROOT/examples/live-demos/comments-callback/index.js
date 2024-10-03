e981437ed17ec8f548f62d0371584d6ca8847a9d
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2024-10-03 14:01:26 +1000

    Update modules/ROOT/examples/live-demos/comments-callback/index.js

--- modules/ROOT/examples/live-demos/comments-callback/index.js
+++ modules/ROOT/examples/live-demos/comments-callback/index.js
@@ -631,12 +631,6 @@ tinymce.ScriptLoader.loadScripts(
       },
       body: JSON.stringify({ uids: conversationUids }),
     })
-      // .then(response => {
-      //   if (!response.ok) {
-      //     throw new Error('Network response was not ok');
-      //   }
-      //   return response.json();
-      // })
       .then((data) => {
         console.log(`Lookup success ${conversationUids}`, data);
         done(data);

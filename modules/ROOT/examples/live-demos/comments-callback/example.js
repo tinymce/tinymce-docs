90b4063cfb950ca2a6080710f81f4cb76fac9df5
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2024-10-08 16:06:38 +1000

    DOC-2524: Update: Callback Mode (Comments) (#3458)
    * DOC-2524: Callback Mode (Comments)
    
    * Revert live_demo_url from testing.
    
    * Update modules/ROOT/examples/live-demos/comments-callback/example.js
    
    Co-authored-by: Hamza Benkhaldoun <hamza0867@gmail.com>
    
    * Update modules/ROOT/examples/live-demos/comments-callback/index.js
    
    Co-authored-by: Hamza Benkhaldoun <hamza0867@gmail.com>
    
    * Update modules/ROOT/partials/configuration/tinycomments_delete_comment.adoc
    
    Co-authored-by: Hamza Benkhaldoun <hamza0867@gmail.com>
    
    * Update modules/ROOT/partials/configuration/tinycomments_fetch.adoc
    
    Co-authored-by: Hamza Benkhaldoun <hamza0867@gmail.com>
    
    * Update modules/ROOT/pages/comments-callback-mode.adoc
    
    * Update modules/ROOT/pages/comments-callback-mode.adoc
    
    * DOC-2524: move tinycomments_fetch to optional options, updated examples to reflect optional callback.
    
    * DOC-2524: fix optional options header level.
    
    * Update modules/ROOT/partials/configuration/tinycomments_lookup.adoc
    
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>
    
    * Update modules/ROOT/examples/live-demos/comments-callback/example.js
    
    * Update modules/ROOT/examples/live-demos/comments-callback/index.js
    
    * Update modules/ROOT/partials/configuration/tinycomments_resolve.adoc
    
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>
    
    * DOC-2524: Update comments callback demo
    
    * Update modules/ROOT/partials/configuration/tinycomments_reply.adoc
    
    Co-authored-by: Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
    
    * Update modules/ROOT/pages/comments-callback-mode.adoc
    
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>
    
    * Update modules/ROOT/pages/comments-callback-mode.adoc
    
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>
    
    * DOC-2524: update example.js and tinycomments_fetch example to match demo updates.
    
    * Update modules/ROOT/pages/comments-callback-mode.adoc
    
    Co-authored-by: Farzad Hayat <farzad.hayatbakhsh@tiny.cloud>
    
    * Update modules/ROOT/pages/comments-callback-mode.adoc
    
    Co-authored-by: Farzad Hayat <farzad.hayatbakhsh@tiny.cloud>
    
    * DOC-2524: Add initial comments to callback demo
    
    * DOC-2524: Fix initialDB structure
    
    * Update modules/ROOT/examples/live-demos/comments-callback/index.js
    
    Co-authored-by: Farzad Hayat <farzad.hayatbakhsh@tiny.cloud>
    
    * Update modules/ROOT/examples/live-demos/comments-callback/example.js
    
    Co-authored-by: Farzad Hayat <farzad.hayatbakhsh@tiny.cloud>
    
    ---------
    
    Co-authored-by: Hamza Benkhaldoun <hamza0867@gmail.com>
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>
    Co-authored-by: Farzad Hayat <farzad.hayatbakhsh@tiny.cloud>

--- modules/ROOT/examples/live-demos/comments-callback/example.js
+++ modules/ROOT/examples/live-demos/comments-callback/example.js
@@ -169,15 +169,47 @@ const tinycomments_lookup = ({ conversationUid }, done, fail) => {
     });
 };
 
+const tinycomments_fetch = (conversationUids, done, fail) => {
+  const requests = conversationUids.map((uid) => fetch(`https://api.example/conversations/${uid}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      }})
+      .then((response) => response.json())
+      .then((data) => ({
+        [uid]: {
+          uid: uid,
+          comments: data
+        }
+      }))
+    );
+
+  Promise.all(requests)
+    .then((data) => {
+      console.log('data', data);
+      const conversations = data.reduce((conv, d) => ({
+          ...conv,
+          ...d
+        })
+      , {});
+      console.log(`Fetch success ${conversationUids}`, conversations);
+      done({ conversations });
+    })
+    .catch((err) => {
+      console.error(`Fetch failure ${conversationUids}`, err);
+      fail('Fetching conversations failed');
+    });
+};
+
 tinymce.init({
   selector: 'textarea#comments-callback',
   height: 800,
   plugins: 'code tinycomments help lists',
   toolbar:
-    'undo redo | blocks | ' +
+    'addcomment showcomments | undo redo | blocks | ' +
     'bold italic backcolor | alignleft aligncenter ' +
     'alignright alignjustify | bullist numlist outdent indent | ' +
-    'removeformat | addcomment showcomments | help',
+    'removeformat | help',
   menubar: 'file edit view insert format tc',
   menu: {
     tc: {
@@ -192,10 +224,6 @@ tinymce.init({
   tinycomments_delete_all,
   tinycomments_delete_comment,
   tinycomments_lookup,
-  /* The following setup callback opens the comments sidebar when the editor loads */
-  setup: (editor) => {
-    editor.on('SkinLoaded', () => {
-      editor.execCommand('ToggleSidebar', false, 'showcomments');
-    });
-  },
+  tinycomments_fetch,
+  sidebar_show: 'showcomments',
 });

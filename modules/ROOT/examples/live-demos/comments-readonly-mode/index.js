c2711b62930832c70cf873672b081496a7d302f8
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2024-10-08 13:26:54 +1000

    DOC-2524: New `tinycomments` option: Comments can be made and read, but the editor is in a readonly state. (#3461)
    * DOC-2524: New comments option: Comments can be made and read, but the editor is in a readonly state.
    
    * DOC-2524: revert live_demo testing url.
    
    * Update modules/ROOT/examples/live-demos/comments-readonly-mode/index.js
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/examples/live-demos/comments-readonly-mode/index.js
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: shirqa <43625458+ShiridiGandham@users.noreply.github.com>
    
    * Update modules/ROOT/pages/comments-options.adoc
    
    Co-authored-by: shirqa <43625458+ShiridiGandham@users.noreply.github.com>
    
    * Update modules/ROOT/examples/live-demos/comments-readonly-mode/index.js
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: shirqa <43625458+ShiridiGandham@users.noreply.github.com>
    
    * DOC-2524: file name change, remove button from demo and update headers.
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Shan <shanmen.ng@tiny.cloud>
    
    * DOC-2524: Restructured tinycomments_access.adoc, removed basic config.
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: shirqa <43625458+ShiridiGandham@users.noreply.github.com>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: shirqa <43625458+ShiridiGandham@users.noreply.github.com>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/partials/configuration/comments-tinycomments_access.adoc
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    
    * Update modules/ROOT/examples/live-demos/comments-ui-mode/index.html
    
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>
    
    ---------
    
    Co-authored-by: Mitchell Crompton <mitchell.crompton@tiny.cloud>
    Co-authored-by: shirqa <43625458+ShiridiGandham@users.noreply.github.com>
    Co-authored-by: Shan <shanmen.ng@tiny.cloud>
    Co-authored-by: tiny-ben-tran <ben.tran@tiny.cloud>

---
+++ modules/ROOT/examples/live-demos/comments-readonly-mode/index.js
@@ -0,0 +1,26 @@
+tinymce.init({
+  selector: "textarea#comment-readonly-ui-mode",
+  plugins: [
+    "tinycomments", "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
+    "help", "image", "insertdatetime", "link", "lists", "media",
+    "preview", "searchreplace", "table", "visualblocks",
+  ],
+  toolbar: "addcomment showcomments togglereadonly | undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  tinycomments_mode: 'embedded',
+  readonly: true,
+  setup: (editor) => {
+    const isReadonlyMode = () => editor.mode.get() === 'readonly';
+    editor.ui.registry.addToggleButton('togglereadonly', {
+      text: 'Readonly mode',
+      context: 'any', // Available from 7.4
+      onSetup: (buttonApi) => {
+        const activate = (api) =>  () => api.setActive(isReadonlyMode());
+        editor.on('SwitchMode', activate(buttonApi));
+        return (teardownApi) => editor.off('SwitchMode', activate(teardownApi));
+      },
+      onAction: (api) => {
+        editor.mode.set(isReadonlyMode() ? 'design' : 'readonly');
+      }
+    });
+  }
+});

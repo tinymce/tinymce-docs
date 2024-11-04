b39adde27723508dadebf3c86b1163595c04aa24
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2024-11-04 13:54:43 +1000

    DOC-2562: update html within demos and include some converter options.

--- modules/ROOT/examples/live-demos/exportpdf/index.js
+++ modules/ROOT/examples/live-demos/exportpdf/index.js
@@ -2,9 +2,17 @@ tinymce.init({
   selector: 'textarea#exportpdf',
   height: '800px',
   plugins: [
-    "exportpdf", "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
+    "exportpdf", "advlist", "anchor", "autolink", "charmap", "code", "codesample", "fullscreen",
     "help", "image", "insertdatetime", "link", "lists", "media",
     "preview", "searchreplace", "table", "visualblocks",
-],
-toolbar: "undo redo | exportpdf | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  ],
+  toolbar: "undo redo | exportpdf | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  image_caption: true,
+  exportpdf_converter_options: {
+    format: 'A4',
+    margin_top: '1in',
+    margin_right: '1in',
+    margin_bottom: '1in',
+    margin_left: '1in'
+  }
 });
--- modules/ROOT/examples/live-demos/exportword/index.js
+++ modules/ROOT/examples/live-demos/exportword/index.js
@@ -5,6 +5,17 @@ tinymce.init({
     "exportword", "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
     "help", "image", "insertdatetime", "link", "lists", "media",
     "preview", "searchreplace", "table", "visualblocks",
-],
-toolbar: "undo redo | exportword | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  ],
+  toolbar: "undo redo | exportword | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  exportword_converter_options: {
+    document: {
+      size: 'A4',
+      margins: {
+        top: "1in",
+        bottom: "1in",
+        left: "1in",
+        right: "1in"
+      }
+    }
+  }
 });

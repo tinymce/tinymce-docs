a26c44cb0eb9b056e251c17b1024c41d2eba67ef
Farzad Hayatbakhsh <farzad.hayatbakhsh@tiny.cloud>
2024-12-21 01:06:59 +1000

    DOC-2608: Populate RSS with changelog.adoc content

--- rssfeed.cjs
+++ rssfeed.cjs
@@ -1,50 +1,79 @@
 "use strict";
 
+const cheerio = require("cheerio");
+
 module.exports.register = function () {
     this.on("beforePublish", ({ siteCatalog, contentCatalog, playbook }) => {
-        const siteLink = playbook.site.url;
+        // Find the changelog page
+        const page = contentCatalog.findBy({ basename: "changelog.adoc" })[0];
 
-        // Find page
-        const page = contentCatalog.findBy({
-            basename: "changelog.adoc",
-        })[0];
+        if (!page) {
+            console.error("changelog.adoc page not found.");
+            return;
+        }
 
-        // Get page attributes
-        const attributes = page.asciidoc.attributes;
+        // Destructure page attributes
         const {
             productname: productName,
             productmajorversion: productMajorVersion,
             doctitle: pageTitle,
             description: pageDescription,
             docname: pageName,
-        } = attributes;
+            "page-component-name": pageComponentName,
+            "page-component-version": pageComponentVersion,
+        } = page.asciidoc.attributes;
 
-        // Get page content
-        const content = page.contents.toString();
+        // Construct site links
+        const siteLink = playbook.site.url;
+        const siteLinkWithVersion = `${siteLink}/${pageComponentName}/${pageComponentVersion}`;
 
-        console.log("Attributes:", attributes);
-        console.log("Content:", content);
+        // Load page content with cheerio
+        const $ = cheerio.load(page.contents.toString());
 
-        // Create RSS feed
+        // Extract releases
+        const releases = $(".sect1")
+            .map((_, element) => {
+                const $element = $(element);
+                const linkElement = $element.find("a.xref");
+                const [version, date] = linkElement.text().split(" - ");
+                return {
+                    title: linkElement.text(),
+                    link: `${siteLinkWithVersion}/${linkElement
+                        .attr("href")
+                        .replace("../", "")}`,
+                    description: `Release notes for TinyMCE ${version}`,
+                    guid: version,
+                    pubDate: new Date(date).toUTCString(),
+                    content: $element.find(".sectionbody").html(),
+                };
+            })
+            .get();
+
+        // Generate RSS feed items
+        const rssItems = releases
+            .map(
+                ({ title, link, description, guid, pubDate, content }) => `
+        <item>
+            <title>${title}</title>
+            <link>${link}</link>
+            <description>${description}</description>
+            <guid isPermaLink="false">${guid}</guid>
+            <pubDate>${pubDate}</pubDate>
+            <content:encoded><![CDATA[${content}]]></content:encoded>
+        </item>`
+            )
+            .join("\n");
+
+        // Assemble the complete RSS feed
         const rss = `<?xml version="1.0" encoding="UTF-8" ?>
-<rss version="2.0">
-
-<channel>
-  <title>${productName} ${productMajorVersion} ${pageTitle}</title>
-  <link>${siteLink}/${pageName}</link>
-  <description>${pageDescription}</description>
-  <item>
-    <title>RSS Tutorial</title>
-    <link>https://www.w3schools.com/xml/xml_rss.asp</link>
-    <description>New RSS tutorial on W3Schools</description>
-  </item>
-  <item>
-    <title>XML Tutorial</title>
-    <link>https://www.w3schools.com/xml</link>
-    <description>New XML tutorial on W3Schools</description>
-  </item>
-</channel>
-
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+    <channel>
+        <title>${productName} ${productMajorVersion} ${pageTitle}</title>
+        <link>${siteLinkWithVersion}/${pageName}</link>
+        <description>${pageDescription}</description>
+        <atom:link href="${siteLink}/rss.xml" rel="self" type="application/rss+xml" />
+        ${rssItems}
+    </channel>
 </rss>`;
 
         // Add RSS feed to site catalog

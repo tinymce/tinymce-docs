6f5286604c537be2d6f9377fc10c8c187906f37e
Sorita Heng <sorita.heng@tiny.cloud>
2025-04-15 10:02:52 +1000

    Fix demo for Review Permissions

--- modules/ROOT/examples/live-demos/suggestededits-access/index.js
+++ modules/ROOT/examples/live-demos/suggestededits-access/index.js
@@ -1,10 +1,38 @@
+/** Fake user database */
+const userinfos = {
+  adamhenderson: {
+      uid: 'adamhenderson',
+      name: 'Adam Henderson',
+      avatar: `https://randomuser.me/api/portraits/men/1.jpg`,
+  },
+  michaelcook: {
+      uid: 'michaelcook',
+      name: 'Michael Cook',
+      avatar: `https://randomuser.me/api/portraits/men/2.jpg`,
+  },
+  kalebwilson: {
+      uid: 'kalebwilson',
+      name: 'Kaleb Wilson',
+      avatar: `https://randomuser.me/api/portraits/men/3.jpg`,
+  },
+  kyleeinstein: {
+      uid: 'kyleeinstein',
+      name: 'Kyle Einstein',
+      avatar: `https://randomuser.me/api/portraits/men/4.jpg`,
+  },
+};
+
+const trackchanges_user_lookup = (uid) => new Promise((resolve, reject) =>
+  setTimeout(() => userinfos[uid] ? resolve(userinfos[uid]) : reject(), 1000));
+
 tinymce.init({
-  selector: 'textarea#trackchanges',
+  selector: 'textarea#trackchanges_access',
   height: 500,
   plugins: 'trackchanges',
   toolbar: 'trackchanges',
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }', 
   trackchanges_uid: 'adamhenderson',
   trackchanges_access: 'read', //change this value to set permission to the Suggested Edits view
-  readonly: false //set to true to restrict a user's editing permission
+  readonly: false, //set to true to restrict a user's editing permission
+  trackchanges_user_lookup
 });

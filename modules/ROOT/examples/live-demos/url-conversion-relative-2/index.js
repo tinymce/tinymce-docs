ccff857e7943d3904338bb7a1c5e6da03514ec77
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2025-07-02 22:11:19 +1000

    DOC-3147: Removed  property from Editor instances, use  instead. Add distinction between tinymce.documentBaseURL and editor.documentBaseURI Expand documentBaseURI documentation with accurate method examples Remove incorrect API method examples Add detailed path resolution explanations Retain documentBaseURL in TinyMCE root API docs

--- modules/ROOT/examples/live-demos/url-conversion-relative-2/index.js
+++ modules/ROOT/examples/live-demos/url-conversion-relative-2/index.js
@@ -3,6 +3,11 @@ tinymce.init({
   height: 300,
   plugins: 'link image code',
   relative_urls: true,
-  document_base_url: '//www.tiny.cloud/docs/demo',
+  // The documentBaseURI will be set to the current page's URL
+  setup: function(editor) {
+    editor.on('init', function() {
+      console.log('Base URI:', editor.documentBaseURI.getURI());
+    });
+  },
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });

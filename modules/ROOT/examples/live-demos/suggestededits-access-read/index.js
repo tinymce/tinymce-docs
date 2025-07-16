767d81fcb2f156f1770797fd90282548e83ed021
MitchCTiny <mitchell.crompton@tiny.cloud>
2025-07-16 20:51:46 +1000

    DOC-3246: Improve examples and access option

--- modules/ROOT/examples/live-demos/suggestededits-access-read/index.js
+++ modules/ROOT/examples/live-demos/suggestededits-access-read/index.js
@@ -420,7 +420,7 @@ const model = {
 };
 
 tinymce.init({
-  selector: 'textarea#suggestededits',
+  selector: 'textarea#suggestededits-access-read',
   height: 500,
   plugins: 'suggestededits advlist anchor autolink code charmap emoticons fullscreen help image link lists media preview searchreplace table',
   toolbar: 'undo redo | suggestededits | styles fontsizeinput | bold italic | align bullist numlist | table link image | code',
@@ -432,5 +432,6 @@ tinymce.init({
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
   suggestededits_model: model,
   suggestededits_content: 'html',
-  suggestededits_access: 'full'
+  suggestededits_access: 'read', // Change this value to set  the user's permission to the Suggested Edits view
+  readonly: false, // Set to true to restrict a user's editing permission
 });
--- modules/ROOT/examples/live-demos/suggestededits/example.js
+++ modules/ROOT/examples/live-demos/suggestededits/example.js
@@ -1,4 +1,7 @@
-const config = {
+const tinymceElement = document.querySelector('textarea#suggested-edits');
+const model = tinymceElement.getAttribute('suggestededits-model');
+
+tinymce.init({
   selector: 'textarea#suggested-edits',
   height: 500,
   plugins: 'suggestededits advlist anchor autolink code charmap emoticons fullscreen help image link lists media preview searchreplace table',
@@ -11,30 +14,7 @@ const config = {
       .catch(() => ({ id: userId })) // Still return a valid user object even if the fetch fails
   )),
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }', 
-}
-
-const loadTinymce = async () => {
-	const documentPromise = fetch(`/documents/${documentId}`); // Fetch the document from the server
-	const modelPromise = fetch(`/models/${documentId}`); // Fetch the model from the same server
-
-	await documentPromise
-    .then((response) => response.text())
-    .then(async (doc) => {
-      const tinymceElement = document.querySelector('div#tinymce');
-      tinymceElement.innerHTML = doc;
-      
-      await modelPromise
-        .then((response) => response.json())
-        .then((model) => {
-          tinymce.init({
-            ...config,
-            suggestededits_model: model,
-          });
-        })
-        .catch(() => {
-          tinymce.init(config); // Initialize without a model if the model fetch fails
-        });
-    });
-};
-
-loadTinymce();
+  suggestededits_model: model,
+  suggestededits_access: 'full',
+  suggestededits_content: 'html'
+});

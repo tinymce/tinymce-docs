60dafc26d5dbd765f1afda66442ee266ea2eb6a7
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2025-08-15 16:47:11 +1000

    DOC-2619: Update landing page, context, and demo to include default config. (#3820)

--- modules/ROOT/examples/live-demos/default-editor/index.js
+++ modules/ROOT/examples/live-demos/default-editor/index.js
@@ -1,3 +1,9 @@
 tinymce.init({
-  selector: 'textarea#default-editor'
+  selector: 'textarea#default-editor',
+  plugins: [
+      "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
+      "help", "image", "insertdatetime", "link", "lists", "media",
+      "preview", "searchreplace", "table", "visualblocks",
+  ],
+  toolbar: "undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
 });

23e91b871e76fddd8d7a0fa1fd86f9b4d9d13f3a
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2025-08-28 11:52:26 +1000

    DOC-3209: Update plugin name, file-names and improve examples.

--- modules/ROOT/examples/live-demos/fullpage/demo.js
+++ modules/ROOT/examples/live-demos/fullpage/demo.js
@@ -1,16 +1,17 @@
 tinymce.init({
-  selector: 'textarea#fullpage',
+  selector: 'textarea#fullpagehtml',
   height: '800px',
   plugins: [
-    "fullpage", "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
+    "fullpagehtml", "advlist", "anchor", "autolink", "charmap", "code", "fullscreen",
     "help", "image", "insertdatetime", "link", "lists", "media",
     "preview", "searchreplace", "table", "visualblocks",
   ],
   menubar: 'file',
-  toolbar: "undo redo | fullpage | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-  // Full Page Plugin Configuration Options
-  fullpage_default_doctype: '<!DOCTYPE html>',  // Has default in source code
-  fullpage_default_title: 'Full Page Plugin Demo Document',  // No default in source code
-  fullpage_default_encoding: 'UTF-8',  // No default in source code
-  fullpage_default_body_style: 'margin: 20px; padding: 15px; font-family: Georgia, Times, serif; font-size: 16px; color: #2c3e50;'  // No default in source code
+  toolbar: "undo redo | fullpagehtml | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  // Full Page HTML Plugin Configuration Options
+  fullpagehtml_default_doctype: '<!DOCTYPE html>',
+  fullpagehtml_default_title: 'Full Page HTML Plugin Demo Document',
+  fullpagehtml_default_encoding: 'UTF-8',
+  fullpagehtml_default_body_style: 'margin: 20px; padding: 15px; font-family: Georgia, Times, serif; font-size: 16px; color: #2c3e50;',
+  fullpagehtml_hide_in_source_view: false  // Show full page HTML in source view
 });

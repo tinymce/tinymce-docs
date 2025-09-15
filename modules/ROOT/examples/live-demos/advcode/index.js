9458ebee838f2b76259731abeac6f7ce7291f083
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2025-09-15 14:41:07 +1000

    DOC-2626: `Documentation Enhancements`: Enhanced Code Editor feature page. (#3856)
    * DOC-2626: Documentation Enhancements: Enhanced Code Editor feature page.
    
    * DOC-2626: remove Overwritten property in live demo.

--- modules/ROOT/examples/live-demos/advcode/index.js
+++ modules/ROOT/examples/live-demos/advcode/index.js
@@ -1,15 +1,25 @@
 tinymce.init({
   selector: 'textarea.advcodedemo',
-  plugins: 'advcode',
-  toolbar: 'code',
+  plugins: [
+    "advcode", "advlist", "anchor", "autolink", "charmap", "fullscreen",
+    "help", "image", "insertdatetime", "link", "lists", "media",
+    "preview", "searchreplace", "table", "visualblocks",
+  ],
+  toolbar: "code | undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
   height: 600,
+  width: '100%',
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });
 
 tinymce.init({
   selector: 'textarea.codedemo',
-  plugins: 'code',
-  toolbar: 'code',
+  plugins: [
+    "code", "advlist", "anchor", "autolink", "charmap", "fullscreen",
+    "help", "image", "insertdatetime", "link", "lists", "media",
+    "preview", "searchreplace", "table", "visualblocks",
+  ],
+  toolbar: "code | undo redo | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
   height: 600,
+  width: '100%',
   content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
 });

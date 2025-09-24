e93c427b32d2effeabba6604e58fcfff68e8eb11
MitchCTiny <mitchell.crompton@tiny.cloud>
2025-09-24 15:08:10 +1000

    TINY-12258: Add documentation for new `suggestededits_auto_approve` option

---
+++ modules/ROOT/examples/live-demos/suggestededits-auto-approve/index.js
@@ -0,0 +1,24 @@
+const API_URL = 'https://demouserdirectory.tiny.cloud/v1/users';
+
+const tinymceElement = document.querySelector('textarea#suggestededits-auto-approve');
+const model = JSON.parse(tinymceElement.getAttribute('suggestededits-model'));
+
+tinymce.init({
+  selector: 'textarea#suggestededits-auto-approve',
+  height: 500,
+  plugins: 'suggestededits advlist anchor autolink code charmap emoticons fullscreen help image link lists media preview searchreplace table',
+  toolbar: 'undo redo | suggestededits | styles fontsizeinput | bold italic | align bullist numlist | table link image | code',
+  content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }',
+  
+  suggestededits_model: model,
+  suggestededits_access: 'full',
+  suggestededits_content: 'html',
+  suggestededits_auto_approve: true,
+
+  user_id: 'kai-nakamura',
+  fetch_users: (userIds) => Promise.all(userIds
+    .map((userId) =>
+      fetch(`${API_URL}/${userId}`)
+        .then((response) => response.json())
+        .catch(() => ({ id: userId })))),
+});

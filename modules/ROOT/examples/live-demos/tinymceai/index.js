ad7fd2a5ff32ecb9f399997b4587d61134fbf008
Karl Kemister-Sheppard <karlkemistersheppard@gmail.com>
2026-02-02 16:18:34 +1000

    DOC-3356: Initial file setup.

---
+++ modules/ROOT/examples/live-demos/tinymceai/index.js
@@ -0,0 +1,6 @@
+tinymce.init({
+  selector: 'textarea#tinymceai',
+  height: '800px',
+  plugins: ["tinymceai", "advlist", "anchor", "autolink", "charmap", "code"],
+  toolbar: "undo redo | tinymceai | styles | bold italic underline strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});
